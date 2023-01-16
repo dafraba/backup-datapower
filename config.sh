9b407beeee3e0ca1efcfba26105fb7c59426c635
Arturo <dvdfraba@gmail.com>
2023-01-16 11:20:50 -0600

    Se actualizó la ip del archivo config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,5 @@
 you can change this information with data from your datapower (s).
 USERNAME="scripts";
 PASSWORD="Prueba123.";
-REST=192.168.1.86:5554;
-XML=192.168.1.86:5550;
+REST=192.168.1.78:5554;
+XML=192.168.1.78:5550;
