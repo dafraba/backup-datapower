dd0e16af3eae702b44fee5606f8bd4e3d56bdba8
Arturo <dvdfraba@gmail.com>
2023-01-16 11:28:26 -0600

    Se agregaron comentarios al archivo config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,4 @@
-you can change this information with data from your datapower (s).
-USERNAME="scripts";
-PASSWORD="Prueba123.";
-REST=192.168.1.78:5554;
-XML=192.168.1.78:5550;
+USERNAME="scripts"; #this user must exist at your datapower device with privileged access level
+PASSWORD="Prueba123."; #change the password as you configured at your datapower device
+REST=192.168.1.78:5554; #change the rest management interface ip address with yours
+XML=192.168.1.78:5550; #change the xml management interface ip address with yours
